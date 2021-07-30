f5815988e3ae8aa7ca6f93833a28c55cd212e7c2
Mateus Fugita <mateushiwao@gmail.com>
2021-07-30 00:48:04 -0300

    feat: filter places by main categories

---
+++ config/config.py
@@ -0,0 +1,14 @@
+categories_dict = {
+    'Parque': ['parque', 'parques de diversões', 'ar livre', 'bonde', 'natureza', 'trilha'],
+    'Museu': ['museus', 'locais históricos', 'arte', 'religioso', 'igreja'],
+    'Cinema': ['cinema'],
+    'Shopping': ['shopping', 'lojas especializadas', 'outlet', 'calçadões'],
+    'Bar': ['bar', 'bares', 'cervejarias', 'balada'],
+    'Show': ['show'],
+    'Biblioteca': ['biblioteca'],
+    'Estádio': ['estádio', 'arenas', 'esportivo'],
+    'Jogos': ['jogos', 'boliche', 'entretenimento', 'stand de tiro'],
+    'Teatro': ['teatro']
+}
+# Excursões gastronômicas
+# Feiras livres

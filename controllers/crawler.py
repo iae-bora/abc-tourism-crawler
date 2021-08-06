9082ebcc71860af19259bc8552963e0d4d932991
Mateus Fugita <mateushiwao@gmail.com>
2021-08-05 22:52:06 -0300

    feat: web scrape restaurant pages from each city

--- controllers/crawler.py
+++ controllers/crawler.py
@@ -5,65 +5,110 @@ from config import Config
 
 MAX_PAGES = 2
 
-def web_scrape_city_pages(url, driver):
-    places = []
+class Crawler:
+    def __init__(self):
+        self.card_wrapper_list_xpath = ""
+        self.next_page_xpath = ""
+        self.categories_xpath = ""
+        self.categories_dictionary = None
+        self.place_name_xpath = ""
+        self.place_image_xpath = ""
+        self.image_property = ""
+    
+    def web_scrape_pages(self, url, driver):
+        places = []
 
-    driver.get(url)
-    check_cookies_banner_exists(driver)
+        driver.get(url)
+        self.check_cookies_banner_exists(driver)
 
-    try:
-        for page in range(0, MAX_PAGES):
+        try:
+            for page in range(0, MAX_PAGES):
+                time.sleep(Config.SLEEP_INTERVAL)
+                card_wrapper_list = driver.find_elements_by_xpath(self.card_wrapper_list_xpath)
+                places.extend(self.iterate_cards(card_wrapper_list))
+                
+                time.sleep(Config.SLEEP_INTERVAL)
+                driver.find_element_by_xpath(self.next_page_xpath).click()
+        except NoSuchElementException:
+            print('No more pages found')
+        except Exception as e:
+            print('Error: ' + str(e))
+        
+        return places
+
+    def check_cookies_banner_exists(self, driver):
+        try:
             time.sleep(Config.SLEEP_INTERVAL)
-            card_containers = driver.find_elements_by_xpath(".//div[@data-automation='cardWrapper']")
-            places.extend(iterate_cards(card_containers))
-            
-            time.sleep(Config.SLEEP_INTERVAL)
-            driver.find_element_by_xpath("//a[@aria-label='Próxima página']").click()
-    except NoSuchElementException:
-        print('No more pages found')
-    except Exception as e:
-        print('Error: ' + str(e))
+            driver.find_element_by_xpath("//button[@class='evidon-banner-acceptbutton']").click()
+        except:
+            print('No cookies banner found')
+        return
+
+    def iterate_cards(self, cards):
+        places_in_page = []
+        for card_index in range(len(cards)):
+            place = self.get_information_of_place(cards[card_index])
+            if place != None:
+                places_in_page.append(place)
+        
+        return places_in_page
+
+    def get_information_of_place(self, card):
+        place_information = {}
+
+        categories_from_tripadvisor = card.find_element_by_xpath(self.categories_xpath).text.split(' • ') or []
+
+        place_information['category'] = ''
+        for category_key, categories_variations in self.categories_dictionary.items():
+            if any(category in ','.join(categories_from_tripadvisor).lower() for category in categories_variations):
+                place_information['category'] = category_key
+                break
+        
+        if place_information['category'] == '':
+            return None
+
+        place_information['name'] = card.find_element_by_xpath(self.place_name_xpath).text.split('.', 1)[-1].strip()
+        place_information['image'] = self.get_image_from_card(card)
+
+        return place_information
+
+
+class PlacesCrawler(Crawler):
+    def __init__(self):
+        self.card_wrapper_list_xpath = ".//div[@data-automation='cardWrapper']"
+        self.next_page_xpath = "//a[@aria-label='Próxima página']"
+        self.categories_xpath = ".//span/div/div/div[2]/div[2]/div[1]/div/div/div[1]"
+        self.categories_dictionary = Config.CATEGORIES_DICT
+        self.place_name_xpath = ".//span[@name='title']"
+        self.place_image_location = "img"
+        self.image_property = "src"
     
-    return places
+    def get_image_from_card(self, card):
+        image = ''
+        try:
+            image = card.find_element_by_tag_name(self.place_image_location).get_attribute(self.image_property)
+        except NoSuchElementException:
+            image = ''
+        return image
 
-def check_cookies_banner_exists(driver):
-    try:
-        time.sleep(Config.SLEEP_INTERVAL)
-        driver.find_element_by_xpath("//button[@class='evidon-banner-acceptbutton']").click()
-    except:
-        print('No cookies banner found')
-    return
-
-def iterate_cards(cards):
-    places_in_page = []
-    for card_index in range(len(cards)):
-        tourist_spot = get_information_of_place(cards[card_index])
-        if tourist_spot != None:
-            places_in_page.append(tourist_spot)
+class RestaurantCrawler(Crawler):
+    def __init__(self):
+        self.card_wrapper_attribute_name = 'data-test'
+        self.card_wrapper_list_xpath = f"//div[contains(@{self.card_wrapper_attribute_name}, '_list_item')]"
+        self.next_page_xpath = "//a[contains(text(), 'Próximas')]"
+        self.categories_xpath = ".//span/div[1]/div[2]/div[2]/div/div[2]/span[1]/span"
+        self.categories_dictionary = Config.RESTAURANT_CATEGORIES_DICT
+        self.place_name_xpath = ".//span/div[1]/div[2]/div[1]/div/span/a"
+        self.place_image_location = ".//span/div[1]/div[1]/span/a/div[2]/div/div[1]/ul/li[1]/div"
+        self.image_property = "background-image"
     
-    return places_in_page
-
-def get_information_of_place(card):
-    place_information = {}
-
-    categories_from_tripadvisor = card.find_element_by_xpath(".//span/div/div/div[2]/div[2]/div[1]/div/div/div[1]").text.split(' • ') or []
-
-    place_information['category'] = ''
-    for category_key, categories_variations in Config.CATEGORIES_DICT.items():
-        if any(category in ','.join(categories_from_tripadvisor).lower() for category in categories_variations):
-            place_information['category'] = category_key
-            break
-    
-    if place_information['category'] == '':
-        return None
-
-    place_information['name'] = card.find_element_by_xpath(".//span[@name='title']").text.split('.', 1)[-1].strip()
-    try:
-        place_information['image'] = card.find_element_by_tag_name('img').get_attribute('src')
-    except NoSuchElementException:
-        place_information['image'] = ''
-
-    return place_information
+    def get_image_from_card(self, card):
+        image = ''
+        try:
+            image = card.find_element_by_xpath(self.place_image_location).value_of_css_property(self.image_property).lstrip('url("').rstrip('")')
+        except NoSuchElementException:
+            image = ''
+        return image
 
 
 # def get_infos(card):

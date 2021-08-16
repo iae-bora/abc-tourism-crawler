9d25a3d6e7db3fe642b328e35d9ed2d730aa7f9b
Mateus Fugita <mateushiwao@gmail.com>
2021-08-16 15:12:14 -0300

    refactor: change table name to singular

--- models/place.py
+++ models/place.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, String, Integer
 from models import Base
 
 class Place(Base):
-    __tablename__ = 'places'
+    __tablename__ = 'place'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)

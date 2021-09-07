f13cb71a009b6cce5a1ed2318d8e44e784e244fb
Mateus Fugita <mateushiwao@gmail.com>
2021-09-07 11:17:29 -0300

    fix: allow places with multiple opening hours in the same day

--- controllers/crawler.py
+++ controllers/crawler.py
@@ -1,8 +1,9 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
 from sqlalchemy import select
 import time as timer
 from datetime import time
-import logging
+import logging, sys
 
 from .places import get_place_details
 from config import Config
@@ -39,6 +40,8 @@ class Crawler:
             for page in range(0, Config.MAX_PAGES_PER_CITY):
                 timer.sleep(Config.SLEEP_INTERVAL)
                 logger.info(f'Page {page} from city {city}')
+                driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME)
+
                 card_wrapper_list = driver.find_elements_by_xpath(self.card_wrapper_list_xpath)
                 self.iterate_cards(card_wrapper_list, city_id)
                 
@@ -61,51 +64,56 @@ class Crawler:
         for card_index in range(len(cards)):
             try:
                 self.get_information_of_place(cards[card_index], city_id)
+                self.db.session.commit()
+                logger.info('Data inserted successfully')
             except Exception as e:
-                logger.error(f'Error retrieving data from place {card_index}: {e}')
+                logger.error(f'Error retrieving data: {e}')
+                self.db.session.rollback()
 
     def get_information_of_place(self, card, city_id):
         place = Place()
 
         place.name = card.find_element_by_xpath(self.place_name_xpath).text.split('.', 1)[-1].strip()
-        logger.info(f"----- Place {place.name} -----")
+        logger.info(f"----- {place.name} -----")
         
         place_already_exist = self.check_if_place_already_exist(place)
         if place_already_exist == True:
-            logger.warning(f'Place {place.name} already exists')
-            return None
+            raise Exception(f'Place {place.name} already exists')
 
         categories_from_tripadvisor = card.find_element_by_xpath(self.categories_xpath).text.split(' • ') or []
 
         place = self.set_category_to_place(categories_from_tripadvisor, place)
         if place == None:
-            logger.warning(f'Category of {place.name} does not match with list')
-            return None
+            raise Exception(f'Category of {place.name} does not match with list')
 
         place.image = self.get_image_from_card(card)
         place.city_id = city_id
 
         place_details = get_place_details(place.name)
         if place_details == {}:
-            logger.error(f'Place {place.name} not found in Google Places')
-            return None
+            raise Exception(f'Place {place.name} not found in Google Places')
+        
         place = self.fill_place_with_details(place, place_details)
 
         if "opening_hours" in place_details:
             place_id = self.insert_place_in_database(place)
-            if place_id is not None:
-                self.save_opening_hours(place_details["opening_hours"]["weekday_text"], place_id)
-                logger.info(f'Opening hours from place {place.name} inserted successfully')
+            if place_id is None:
+                raise Exception(f'Error adding {place.name} to database')
+            
+            self.save_opening_hours(place_details["opening_hours"]["weekday_text"], place_id)
+            logger.info('Opening hours added successfully')
         else:
-            logger.error(f'Place {place.name} does not have opening_hours')
+            raise Exception(f'Place {place.name} does not have opening_hours')
 
         return place
     
     def insert_place_in_database(self, place):
         try:
             self.db.session.add(place)
-            self.db.session.commit()
-            logger.info(f'Place inserted successfully: {place.name}')
+            self.db.session.flush()
+            self.db.session.refresh(place)
+
+            logger.info(f'Place added successfully: {place.name}')
             return place.id
         except Exception as e:
             logger.error(f'Database error when inserting place: {str(e)}')
@@ -155,33 +163,34 @@ class Crawler:
     
     def save_opening_hours(self, weekday_text_list, place_id):
         for weekday_text in weekday_text_list:
-            day_of_week, opening_hour = weekday_text.split(':', 1)
-            opening_hours = OpeningHours()
-            opening_hours.day_of_week = day_of_week
-            
-            if opening_hour.strip() == 'Fechado':
-                opening_hours.open = False
-            else:
-                opening_hours.open = True
+            day_of_week, opening_hour_list = weekday_text.split(':', 1)
 
-                if opening_hour.strip() == 'Atendimento 24 horas':
-                    opening_hours.start_hour = time(0, 0, 0)
-                    opening_hours.end_hour = time(23, 59, 59)
+            for opening_hour in opening_hour_list.split(','):
+                opening_hours = OpeningHours()
+                opening_hours.day_of_week = day_of_week
+                
+                if opening_hour.strip() == 'Fechado':
+                    opening_hours.open = False
                 else:
-                    start_hour, end_hour = opening_hour.strip().split(' – ')
-                    start_hour_hours, start_hour_minutes = start_hour.split(':')
-                    end_hour_hours, end_hour_minutes = end_hour.split(':')
+                    opening_hours.open = True
 
-                    opening_hours.start_hour = time(int(start_hour_hours), int(start_hour_minutes), 0)
-                    opening_hours.end_hour = time(int(end_hour_hours), int(end_hour_minutes), 0)
-            
-            opening_hours.place_id = place_id
-            self.insert_opening_hours_in_database(opening_hours)
+                    if opening_hour.strip() == 'Atendimento 24 horas':
+                        opening_hours.start_hour = time(0, 0, 0)
+                        opening_hours.end_hour = time(23, 59, 59)
+                    else:
+                        start_hour, end_hour = opening_hour.strip().split(' – ')
+                        start_hour_hours, start_hour_minutes = start_hour.split(':')
+                        end_hour_hours, end_hour_minutes = end_hour.split(':')
+
+                        opening_hours.start_hour = time(int(start_hour_hours), int(start_hour_minutes), 0)
+                        opening_hours.end_hour = time(int(end_hour_hours), int(end_hour_minutes), 0)
+                
+                opening_hours.place_id = place_id
+                self.insert_opening_hours_in_database(opening_hours)
     
     def insert_opening_hours_in_database(self, opening_hours):
         try:
             self.db.session.add(opening_hours)
-            self.db.session.commit()
         except Exception as e:
             logger.error(f'Database error when inserting opening_hours: {str(e)}')
         

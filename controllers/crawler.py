39c432666ee46c07984c3970524b1e7c499bf11a
Mateus Fugita <mateushiwao@gmail.com>
2021-09-12 22:28:09 -0300

    fix: retrieve city name from Google Places API

--- controllers/crawler.py
+++ controllers/crawler.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from sqlalchemy import select
 import time as timer
 from datetime import time
-import logging, sys
+import logging
 
 from .places import get_place_details
 from config import Config
@@ -34,8 +34,6 @@ class Crawler:
         driver.get(url)
         self.check_cookies_banner_exists(driver)
 
-        city_id = self.get_id_from_city(city)
-
         try:
             for page in range(0, Config.MAX_PAGES_PER_CITY):
                 timer.sleep(Config.SLEEP_INTERVAL)
@@ -43,7 +41,7 @@ class Crawler:
                 driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME)
 
                 card_wrapper_list = driver.find_elements_by_xpath(self.card_wrapper_list_xpath)
-                self.iterate_cards(card_wrapper_list, city_id)
+                self.iterate_cards(card_wrapper_list)
                 
                 timer.sleep(Config.SLEEP_INTERVAL)
                 driver.find_element_by_xpath(self.next_page_xpath).click()
@@ -60,52 +58,50 @@ class Crawler:
             logger.info('No cookies banner found')
         return
 
-    def iterate_cards(self, cards, city_id):
+    def iterate_cards(self, cards):
         for card_index in range(len(cards)):
-            try:
-                self.get_information_of_place(cards[card_index], city_id)
-                self.db.session.commit()
-                logger.info('Data inserted successfully')
-            except Exception as e:
-                logger.error(f'Error retrieving data: {e}')
-                self.db.session.rollback()
+            self.get_information_of_place(cards[card_index])
 
-    def get_information_of_place(self, card, city_id):
-        place = Place()
+    def get_information_of_place(self, card):
+        try:
+            place = Place()
 
-        place.name = card.find_element_by_xpath(self.place_name_xpath).text.split('.', 1)[-1].strip()
-        logger.info(f"----- {place.name} -----")
-        
-        place_already_exist = self.check_if_place_already_exist(place)
-        if place_already_exist == True:
-            raise Exception(f'Place {place.name} already exists')
-
-        categories_from_tripadvisor = card.find_element_by_xpath(self.categories_xpath).text.split(' • ') or []
-
-        place = self.set_category_to_place(categories_from_tripadvisor, place)
-        if place == None:
-            raise Exception(f'Category of {place.name} does not match with list')
-
-        place.image = self.get_image_from_card(card)
-        place.city_id = city_id
-
-        place_details = get_place_details(place.name)
-        if place_details == {}:
-            raise Exception(f'Place {place.name} not found in Google Places')
-        
-        place = self.fill_place_with_details(place, place_details)
-
-        if "opening_hours" in place_details:
-            place_id = self.insert_place_in_database(place)
-            if place_id is None:
-                raise Exception(f'Error adding {place.name} to database')
+            place.name = card.find_element_by_xpath(self.place_name_xpath).text.split('.', 1)[-1].strip()
+            logger.info(f"----- {place.name} -----")
             
-            self.save_opening_hours(place_details["opening_hours"]["weekday_text"], place_id)
-            logger.info('Opening hours added successfully')
-        else:
-            raise Exception(f'Place {place.name} does not have opening_hours')
+            place_already_exist = self.check_if_place_already_exist(place)
+            if place_already_exist is not False:
+                raise Exception(f'Place {place.name} already exists')
 
-        return place
+            categories_from_tripadvisor = card.find_element_by_xpath(self.categories_xpath).text.split(' • ') or []
+
+            place = self.set_category_to_place(categories_from_tripadvisor, place)
+            if place == None:
+                raise Exception(f'Category of {place.name} does not match with list')
+
+            place.image = self.get_image_from_card(card)
+
+            place_details = get_place_details(place.name)
+            if place_details == {}:
+                raise Exception(f'Place {place.name} not found in Google Places')
+            
+            place = self.fill_place_with_details(place, place_details)
+
+            if "opening_hours" in place_details:
+                place_id = self.insert_place_in_database(place)
+                if place_id is None:
+                    raise Exception(f'Error adding {place.name} to database')
+                
+                self.save_opening_hours(place_details["opening_hours"]["weekday_text"], place_id)
+                logger.info('Opening hours added successfully')
+            else:
+                raise Exception(f'Place {place.name} does not have opening_hours')
+
+            self.db.session.commit()
+            logger.info('Data inserted successfully')
+        except Exception as e:
+            logger.error(f'Error retrieving data: {e}')
+            self.db.session.rollback()
     
     def insert_place_in_database(self, place):
         try:
@@ -144,14 +140,19 @@ class Crawler:
     
     def check_if_place_already_exist(self, new_place):
         statement = select(Place).where(Place.name == new_place.name)
-        result = self.db.session.execute(statement).first()
+        result = self.db.session.execute(statement).scalars().first()
 
         if result is None:
             return False
         else:
-            return True
+            return result
 
     def fill_place_with_details(self, place: Place, details: dict):
+        city = self.get_city_from_address_components(details['address_components'])
+        if city not in Config.CITIES_LIST.keys():
+            raise Exception(f'City {city} is not located in ABC region')
+        place.city_id = self.get_id_from_city(city)
+
         place.business_status = details["business_status"]
         place.address = details["formatted_address"]
         place.phone = details.get("formatted_phone_number", None)
@@ -160,6 +161,13 @@ class Crawler:
         place.longitude = details["geometry"]["location"]["lng"]
 
         return place
+    
+    def get_city_from_address_components(self, address_components):
+        for address_component in address_components:
+            if 'administrative_area_level_2' in address_component['types']:
+                city = address_component['long_name']
+                return city
+        return None
     
     def save_opening_hours(self, weekday_text_list, place_id):
         for weekday_text in weekday_text_list:
